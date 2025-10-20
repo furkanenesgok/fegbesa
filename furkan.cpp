8e84ebc17492174f6076752ac0969ce021e475b5
furkanenesgok <betul123898@gmail.com>
2025-10-20 18:11:41 +0300

    Add files via upload

---
+++ furkan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 10 kez "deneyap" yazdýr
+    for (int i = 0; i < 10; i++) {
+        cout << "deneyap" << endl;
+    }
+
+    // 2 kez "merhaba" yazdýr
+    for (int i = 0; i < 2; i++) {
+        cout << "merhaba" << endl;
+    }
+
+    return 0;
+}
